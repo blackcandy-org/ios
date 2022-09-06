d2776eeefea36a569e90dd5e93f19601c9559b64
aidewoode <aidewoode@gmail.com>
2022-09-06 09:52:24 +0800

    Avoid logs of `maximumViewportInset cannot be larger than frame`

--- BlackCandy/Turbo/TurboSession.swift
+++ BlackCandy/Turbo/TurboSession.swift
@@ -9,6 +9,7 @@ struct TurboSession {
     configuration.applicationNameForUserAgent = "Turbo Native iOS"
     configuration.processPool = TurboSession.processPool
 
-    return Session(webViewConfiguration: configuration)
+    // Set the webview frame more than zero to avoid logs of `maximumViewportInset cannot be larger than frame`
+    return Session(webView: WKWebView(frame: CGRect(x: 0.0, y: 0.0, width: 0.1, height: 0.1), configuration: configuration))
   }
 }
