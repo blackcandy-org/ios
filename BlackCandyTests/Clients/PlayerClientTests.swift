9251a976811ea400d3c4ca79b977dbbe31884055
aidewoode <aidewoode@gmail.com>
2023-05-09 10:44:37 +0800

    Fix tests error in PlayerClient

--- BlackCandyTests/Clients/PlayerClientTests.swift
+++ BlackCandyTests/Clients/PlayerClientTests.swift
@@ -55,7 +55,7 @@ final class PlayerClientTests: XCTestCase {
     playerClient.seek(time)
 
     for await currentTime in playerClient.getCurrentTime().prefix(1) {
-      XCTAssertEqual(currentTime, 2)
+      XCTAssertEqual(currentTime.rounded(), 2)
     }
   }
 
@@ -67,7 +67,7 @@ final class PlayerClientTests: XCTestCase {
     playerClient.replay()
 
     for await currentTime in playerClient.getCurrentTime().prefix(1) {
-      XCTAssertEqual(currentTime, 0)
+      XCTAssertEqual(currentTime.rounded(), 0)
     }
   }
 
@@ -82,7 +82,7 @@ final class PlayerClientTests: XCTestCase {
 
       if status == .playing {
         for await currentTime in playerClient.getCurrentTime().prefix(1) {
-          times.append(currentTime)
+          times.append(currentTime.rounded())
         }
 
         playerClient.stop()
